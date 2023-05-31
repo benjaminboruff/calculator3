854209e06861131c28b7e6802e448c2fc8487f7e
Benjamin H Boruff <11185240+benjaminboruff@users.noreply.github.com>
2023-05-31 18:03:16 -0400

    new build for Netlify deploy

--- dist/calculator3-a4610434cfb174af.js
+++ dist/calculator3-a4610434cfb174af.js
@@ -349,7 +349,7 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper545 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper546 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 317, __wbg_adapter_14);
         return addHeapObject(ret);
     };
@@ -391,7 +391,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('calculator3-e461a55f3846a3e9_bg.wasm', import.meta.url);
+        input = new URL('calculator3-a4610434cfb174af_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
